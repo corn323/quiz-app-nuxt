af345396611cdacafb834b7985a56544cb76ceb7
玉米 <83815808+corn323@users.noreply.github.com>
2024-12-25 23:33:36 +0800

    修改vuetify.ts
    mdi問題導致ICON消失
    顯示報錯為正常現象

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,17 +1,25 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
-    components, directives,
+    components,
+    directives,
+
     icons: {
       defaultSet: 'mdi',
       aliases: {
         home: 'mdi-home',
-      }
+      },
+      sets: {
+        mdi: {
+          // 不需要指定 component
+        },
+      },
     },
   });
 
-  nuxtApp.vueApp.use(vuetify);
-});
+  nuxtApp.vueApp.use(vuetify)
+})
